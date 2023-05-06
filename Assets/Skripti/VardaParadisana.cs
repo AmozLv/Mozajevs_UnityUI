a97f5383442a00671609f0eeee5b12d0b19e41c0
Alekss Možajevs <alekss.mozajevs@sk.lvt.lv>
2023-05-07 01:10:04 +0300

    Izveidots apraksts(scroll view)

--- Assets/Skripti/VardaParadisana.cs
+++ Assets/Skripti/VardaParadisana.cs
@@ -3,31 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VardaParadisana : MonoBehaviour {
+public class VardaParadisana : MonoBehaviour
+{
 	private string vards;
 	private string vecums;
 	public GameObject ievadesLauksVards;
 	public GameObject ievadesLauksVecums;
 	public GameObject tekstaAttelosana;
-	private string[] tekstaSakums = {"Zēns vārdā ","Meitene vārdā "};
-	private string[] tekstaBeigas = {" vecs"," veca"};
+	public Dropdown telaIzvele;
+	public GameObject telaApraksts;
+	public Text scrollTeksts;
 
-	int indekss;
+	private string[] tekstaSakums = { "Zēns vārdā ", "Meitene vārdā " };
+	private string[] tekstaBeigas = { " vecs", " veca" };
 
-	public void UzglabatTekstu(){
-		
-			vards = ievadesLauksVards.GetComponent<InputField> ().text;
-			vecums = ievadesLauksVecums.GetComponent<InputField> ().text;
-		if (vards.Length > 0 || vecums.Length > 0) {
-			char pedejaisBurts = vards.Substring (vards.Length - 1, 1) [0];
+
+	private int indekss;
+
+	void Start()
+	{
+		telaIzvele.value = 0;
+		MainitTelaAprakstu();
+	}
+
+	public void UzglabatTekstu()
+	{
+		vards = ievadesLauksVards.GetComponent<InputField>().text;
+		vecums = ievadesLauksVecums.GetComponent<InputField>().text;
+		if (vards.Length > 0 || vecums.Length > 0)
+		{
+			char pedejaisBurts = vards.Substring(vards.Length - 1, 1)[0];
 			if (pedejaisBurts == 'a' || pedejaisBurts == 'e')
 				indekss = 1;
 			else
 				indekss = 0;
-			
 
-			tekstaAttelosana.GetComponent<Text> ().text = tekstaSakums [indekss] + vards.ToUpper () + " ir " + vecums + tekstaBeigas [indekss] + "!";
-		}else tekstaAttelosana.GetComponent<Text> ().text = "Nav ievadīts vārds/vecums!";
+
+			tekstaAttelosana.GetComponent<Text>().text = tekstaSakums[indekss] + vards.ToUpper() + " ir " + vecums + tekstaBeigas[indekss] + "!";
+		}
+		else tekstaAttelosana.GetComponent<Text>().text = "Nav ievadīts vārds/vecums!";
 	}
-}
+	public void MainitTelaAprakstu()
+	{
+		int izvelesIndekss = telaIzvele.value;
+		if (izvelesIndekss == 0)
+		{
+			scrollTeksts.text = "Zēns kas neko nejēdz no ģērbšanās";
+		}
+		else if (izvelesIndekss == 1)
+		{
+			scrollTeksts.text = "Meitēns kas neko nejēdz no ģērbšanās";
+		}
 
+	}
+
+
+}
