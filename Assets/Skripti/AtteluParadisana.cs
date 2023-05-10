0017a9f61bedd2687411b7dc4a81a3d8d64ae24f
unknown <alekss.mozajevs@sk.lvt.lv>
2023-05-10 11:27:33 +0300

    Darbs v2

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -8,7 +8,26 @@ public class AtteluParadisana : MonoBehaviour {
 	public Sprite[] atteluMasivs;
 	public GameObject garSlaideris;
 	public GameObject platSlaideris;
+	public GameObject AvizeImg;
+	public GameObject PavarImg;
+	public GameObject BantImg;
+	public GameObject VioletKreklImg;
+	public GameObject SirdsKreklImg;
+	public GameObject ZalsKreklImg;
+	public GameObject ToggleCepures;
 
+	public void cepuruAttelosana(bool vertiba){
+		AvizeImg.SetActive (vertiba);
+		PavarImg.SetActive (vertiba);
+		BantImg.SetActive (vertiba);
+
+	}
+	public void apgerbuAttelosana(bool vertiba){
+		VioletKreklImg.SetActive (vertiba);
+		SirdsKreklImg.SetActive (vertiba);
+		ZalsKreklImg.SetActive (vertiba);
+
+	}
 	public void izkritosais(int sk){
 		if (sk == 0)
 			mainigaisAttels.GetComponent<Image> ().sprite = atteluMasivs [0];
@@ -25,6 +44,13 @@ public class AtteluParadisana : MonoBehaviour {
 		Vector2 lielums = mainigaisAttels.transform.localScale;
 		lielums.x = Mathf.Clamp(platumaVertiba, 0f, 2f); // ierobežot lielumu no 0 līdz 2
 		mainigaisAttels.transform.localScale = lielums;
+		ZalsKreklImg.transform.localScale = lielums;
+		SirdsKreklImg.transform.localScale = lielums;
+		VioletKreklImg.transform.localScale = lielums;
+		BantImg.transform.localScale = lielums;
+		PavarImg.transform.localScale = lielums;
+		AvizeImg.transform.localScale = lielums;
+
 	}
 
 	public void MainitGarumu()
@@ -34,5 +60,11 @@ public class AtteluParadisana : MonoBehaviour {
 		Vector2 lielums = mainigaisAttels.transform.localScale;
 		lielums.y = Mathf.Clamp(garumaVertiba, 0f, 2f); // ierobežot lielumu no 0 līdz 2
 		mainigaisAttels.transform.localScale = lielums;
+		ZalsKreklImg.transform.localScale = lielums;
+		SirdsKreklImg.transform.localScale = lielums;
+		VioletKreklImg.transform.localScale = lielums;
+		BantImg.transform.localScale = lielums;
+		PavarImg.transform.localScale = lielums;
+		AvizeImg.transform.localScale = lielums;
 	}
 }
